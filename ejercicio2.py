44daf196f65dddfc4ef0e1715d65c28caab7eb11
alolopezz <alejandro.lopeztoledano@usp.ceu.es>
2022-10-16 20:21:58 +0200

    Tema2_CEU

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,11 @@
+texto = 'un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las bandoleras ni elviento, lo que se mueve son nuestras mentes -dijo el maestro'
+lineas = texto.split('#')
+for i, linea in enumerate(lineas): 
+    lineas[i] = linea.capitalize()
+    if 1 == 0:
+        lineas[i] = lineas[i] + '...'
+    else:
+        lineas[i] = '- ' + lineas[i] + '.'
 
+for linea in lineas: 
+    print(linea)
